f40f1b86d56ef23f95d2ba38cc10a3c009996489
Mateus Chiazi <129672280+CharlyChiazi@users.noreply.github.com>
2024-02-27 16:24:39 +0100

    ohhhh

---
+++ projetosJS/experiences.js
@@ -0,0 +1,10 @@
+var nome = prompt("Digite seu nome");
+var idade = prompt("Digite sua idade");
+
+console.log(idade);
+
+var idade2 = 39;
+console.log(idade2);
+
+//alert(teste);
+document.write("<h3>ola " + nome + " tudo bem? Estou vendo que vc tem "  + idade + " anos de idade!</h3>")
